e13f94c8b619f40265e8d64bcedce68e5ffdd9f1
Andrés Fernández <zhsshz17@gmail.com>
2022-10-25 22:38:14 -0600

    Actualización pdf y Pagos 2.0

--- src/controllers/controller.generarpdf.js
+++ src/controllers/controller.generarpdf.js
@@ -43,7 +43,8 @@ const Pdf = {
             })
             const sumaTotal = arrayTotal.reduce((a, b) => a + b)
             const sumaTotalDecimales = sumaTotal.toFixed(2);
-
+            if (compras) {
+            console.log(compras)
                 const template = `
                   <!doctype html>
     <html>
@@ -104,7 +105,9 @@ const Pdf = {
                 });
 
                 res.status(200).send('exito')
-           
+            }else{
+                res.status(500).send('Error')
+            }
         } catch (e) {
             res.status(500).send('Error')
         }
